f49724c208354d44c93fc028c319d7f9992e0238
Nonthakorn <nonthakornsingthon@gmail.com>
2025-11-23 19:20:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    const studentId = document.getElementById('student-id').textContent;
+    alert(`คุณกำลังศึกษาระดับปริญญาตรี คณะ ICT มหาวิทยาลัยพะเยา\nรหัสนิสิต: ${studentId}`);
+}
+
+console.log("เว็บไซต์แนะนำตัวโหลดสมบูรณ์แล้ว!");
